803ae56f376e2a7720c73bbd0f52e6f38c6ab6c3
David <david@nlsn.nu>
2021-11-09 13:35:25 +0100

    Övning 1.4

--- src/test/java/org/example/CarFactoryTest.java
+++ src/test/java/org/example/CarFactoryTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,12 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class CarFactoryTest {
     @Test
     void test_create_car_success() {
-        CarFactory carFactory = new CarFactory("Saab");
+        VehicleRegistrationNumberGenerator vehicleRegistrationNumberGenerator = new VehicleRegistrationNumberGenerator(List.of("ABC123"));
+        CarFactory carFactory = new CarFactory(vehicleRegistrationNumberGenerator, "Saab");
+
 
         Car car = carFactory.createNewCar("Red");
 
         assertNotNull(car);
         assertEquals("Red", car.getColor());
         assertEquals("Saab", car.getBrand());
+        assertEquals("ABC123", car.getRegNo());
+        System.out.println(car.getRegNo());
     }
 }
